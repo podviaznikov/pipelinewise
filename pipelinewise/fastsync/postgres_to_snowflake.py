cc6024d9e43aecce9803ce3f72f6ae1d07a83aef
Anton Podviaznikov <anton@podviaznikov.com>
2022-07-01 13:18:32 -0400

    Update postgres_to_snowflake.py

--- pipelinewise/fastsync/postgres_to_snowflake.py
+++ pipelinewise/fastsync/postgres_to_snowflake.py
@@ -222,14 +222,7 @@ def main_impl():
     LOGGER.info(
         """
         -------------------------------------------------------
-        SYNC FINISHED - SUMMARY
-        -------------------------------------------------------
-            Total tables selected to sync  : %s
-            Tables loaded successfully     : %s
-            Exceptions during table sync   : %s
-
-            Pool size                      : %s
-            Runtime                        : %s
+        SYNC FINISHED - SUMMARY, Total tables selected to sync  : %s, Tables loaded successfully     : %s, Exceptions during table sync   : %s, Pool size                      : %s, Runtime                        : %s
         -------------------------------------------------------
         """,
         len(args.tables),
